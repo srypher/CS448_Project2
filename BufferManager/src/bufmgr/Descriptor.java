886fea10b44437e5669f43f804f39378587adbd1
mattlazar <lazarm@purdue.edu>
2016-02-18 19:40:16 -0500

    Buffer Manager fix
    one step closer

--- BufferManager/src/bufmgr/Descriptor.java
+++ BufferManager/src/bufmgr/Descriptor.java
@@ -3,53 +3,54 @@ package bufmgr;
 import global.PageId;
 
 public class Descriptor {
-	private PageId pagenumber;
-	private int pin_count;
-	private boolean dirtybit;
-	private int lfucounter;
+ private PageId pagenumber;
+ private int pin_count;
+ private boolean dirtybit;
+ private int lfucounter;
 
-	Descriptor(PageId pagenumber) {
-			this.pagenumber = pagenumber;
-			pin_count = 0;
-			dirtybit = false;
-	}
+ Descriptor(PageId pagenumber) {
+   this.pagenumber = new PageId(pagenumber.pid);
+   pin_count = 0;
+   dirtybit = false;
+   lfucounter = 0;
+ }
 
-	//toggle dirty bit to on
-	public void toggleDirty() {
-		dirtybit = true;
-	}
-	public void untoggleDirty() {
-		dirtybit = false;
-	}
+ //toggle dirty bit to on
+ public void toggleDirty() {
+  dirtybit = true;
+ }
+ public void untoggleDirty() {
+  dirtybit = false;
+ }
 
-	//get dirtybit value;
-	public boolean getDirty() {
-		return dirtybit;
-	}
+ //get dirtybit value;
+ public boolean getDirty() {
+  return dirtybit;
+ }
 
-	//get pagenumber
-	public PageId getPagenumber() {
-		return pagenumber;
-	}
+ //get pagenumber
+ public PageId getPagenumber() {
+  return pagenumber;
+ }
 
-	public int getLFUCount() {
-		return lfucounter;
-	}
+ public int getLFUCount() {
+  return lfucounter;
+ }
 
-	//get pin count
-	public int getPinCount() {
-		return pin_count;
-	}
+ //get pin count
+ public int getPinCount() {
+  return pin_count;
+ }
 
-	//increment the pin count and lfucounter
-	public void pinPage() {
-		pin_count++;
-		lfucounter++;
-	}
+ //increment the pin count and lfucounter
+ public void pinPage() {
+  pin_count++;
+  lfucounter++;
+ }
 
-	//decrement the pin count
-	public void unpinPage() {
-		pin_count--;
-	}
+ //decrement the pin count
+ public void unpinPage() {
+  pin_count--;
+ }
 
 }
